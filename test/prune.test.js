1cffcb281230d9825bcce801787726d1c3d0936b
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-19 09:06:19 -0800

    feat: remove empty media blocks

--- test/prune.test.js
+++ test/prune.test.js
@@ -3,9 +3,19 @@ const path = require('path')
 const prune = require('../lib/prune')
 
 describe('prune.js', function () {
-  it('should work', function () {
+  const cssContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.css'), 'utf8')
+
+  it('should remove unused rules', function () {
     const html = '<div class="foo-bar" id="something"></div>'
-    const css = fs.readFileSync(path.join(__dirname, '/fixtures/content.css'), 'utf8')
-    expect(prune(html, css)).to.have.property('length').greaterThan(10).lessThan(120)
+    const result = prune(html, cssContent)
+    expect(result).to.have.property('length').greaterThan(10).lessThan(120)
+    expect(result).to.not.contain('foobar[something=x]')
+    expect(result).to.not.contain('.something3')
+  })
+
+  it('should remove empty media sets', function () {
+    const html = '<div class="foo-bar" id="something"></div>'
+    const result = prune(html, cssContent)
+    expect(result).to.not.contain('@media')
   })
 })
