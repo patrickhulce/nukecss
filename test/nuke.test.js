ac2e570efc1079b2e07afa84c92a9f9dac680501
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-19 19:21:55 -0800

    feat: improve comment control

--- test/nuke.test.js
+++ test/nuke.test.js
@@ -3,19 +3,22 @@ const path = require('path')
 const nuke = require('../lib/nuke')
 
 describe('nuke.js', function () {
+  const htmlContent = '<div class="foo-bar" id="something"></div>'
   const cssContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.css'), 'utf8')
 
   it('should remove unused rules', function () {
-    const html = '<div class="foo-bar" id="something"></div>'
-    const result = nuke(html, cssContent)
-    expect(result).to.have.property('length').greaterThan(10).lessThan(120)
+    const result = nuke(htmlContent, cssContent)
     expect(result).to.not.contain('foobar[something=x]')
     expect(result).to.not.contain('.something3')
   })
 
   it('should remove empty media sets', function () {
-    const html = '<div class="foo-bar" id="something"></div>'
-    const result = nuke(html, cssContent)
+    const result = nuke(htmlContent, cssContent)
     expect(result).to.not.contain('@media')
+  })
+
+  it('should respect nukecss:* comments', function () {
+    const result = nuke(htmlContent, cssContent)
+    expect(result).to.contain('.totally-unused')
   })
 })
