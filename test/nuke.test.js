496bc200fbd98b3299b21d0a7604f2a9cba674f5
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-20 22:23:23 -0800

    feat: parse javascript sources

--- test/nuke.test.js
+++ test/nuke.test.js
@@ -1,30 +1,81 @@
 const fs = require('fs')
 const path = require('path')
-const nuke = require('../lib/nuke')
+const nukecss = require('../lib/nuke')
 
 describe('nuke.js', function () {
-  const htmlContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.html'), 'utf8')
-  const cssContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.css'), 'utf8')
+  context('when content is basic', () => {
+    const htmlContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.html'), 'utf8')
+    const cssContent = fs.readFileSync(path.join(__dirname, '/fixtures/content.css'), 'utf8')
 
-  it('should remove unused rules', function () {
-    const result = nuke(htmlContent, cssContent)
-    expect(result).to.not.contain('foobar[something=x]')
-    expect(result).to.not.contain('.something3')
-    expect(result).to.not.contain('.foo-bar-3')
+    it('should remove unused rules', function () {
+      const result = nukecss(htmlContent, cssContent)
+      expect(result).to.not.contain('foobar[something=x]')
+      expect(result).to.not.contain('.something3')
+      expect(result).to.not.contain('.foo-bar-3')
+    })
+
+    it('should remove empty media sets', function () {
+      const result = nukecss(htmlContent, cssContent)
+      expect(result).to.not.contain('@media')
+    })
+
+    it('should respect nukecss:* comments', function () {
+      const result = nukecss(htmlContent, cssContent)
+      expect(result).to.contain('.totally-unused')
+    })
+
+    it('should support multiple sources', function () {
+      const result = nukecss([htmlContent, '<div id="foo-bar-3"></div>'], cssContent)
+      expect(result).to.contain('.foo-bar-3')
+    })
   })
 
-  it('should remove empty media sets', function () {
-    const result = nuke(htmlContent, cssContent)
-    expect(result).to.not.contain('@media')
-  })
+  context('when content is parseable', () => {
+    const jsContent = 'const jsignored = "js-class other-class"'
+    const moreJsContent = 'const woah = ["still", "works"].join("-")'
+    const htmlContent = '<div id="primary" class="html-class">html-ignored</div>'
+    const cssContent = `
+      .jsignored { color: white; }
+      .html-ignored { color: white; }
+      .js-class { color: white; }
+      .other-class { color: white; }
+      .still-works { color: white; }
+      .html-class { color: white; }
+      #primary { color: white; }
+      #primary > .unused { color: white; }
+      .also-unused { color: white; }
+    `.replace(/\n\s*/g, '\n')
 
-  it('should respect nukecss:* comments', function () {
-    const result = nuke(htmlContent, cssContent)
-    expect(result).to.contain('.totally-unused')
-  })
+    const nuked = nukecss([
+      {content: jsContent, type: 'js'},
+      {content: moreJsContent, type: 'js'},
+      {content: htmlContent, type: 'html'},
+    ], cssContent)
 
-  it('should support multiple sources', function () {
-    const result = nuke([htmlContent, '<div id="foo-bar-3"></div>'], cssContent)
-    expect(result).to.contain('.foo-bar-3')
+    console.log(nuked)
+
+    it('should remove unused rules', function () {
+      expect(nuked).to.not.contain('#primary > .unused')
+      expect(nuked).to.not.contain('.also-unused')
+    })
+
+    it('should not remove used rules', function () {
+      expect(nuked).to.contain('.js-class')
+      expect(nuked).to.contain('.other-class')
+      expect(nuked).to.contain('.html-class')
+      expect(nuked).to.contain('#primary')
+    })
+
+    it('should remove unused rules mentioned in non-strings', function () {
+      expect(nuked).to.not.contain('jsignored')
+    })
+
+    it.skip('should remove unused rules mentioned in textnodes', function () {
+      expect(nuked).to.not.contain('html-ignored')
+    })
+
+    it('should not remove unused rules dynamically joined', function () {
+      expect(nuked).to.contain('.still-works')
+    })
   })
 })
