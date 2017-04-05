6d993fc63817bbd79815ebb9577697f26377b00f
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-04 19:52:43 -0700

    style: update for lint

--- bin/nukecss.js
+++ bin/nukecss.js
@@ -2,12 +2,11 @@
 
 const fs = require('fs')
 const glob = require('glob')
-const path = require('path')
 const nuke = require('../lib/nuke')
 
 const args = process.argv.slice(2)
 if (args.length !== 2) {
-  console.error(`
+  process.stderr.write(`
     Usage: nukecss <sources> <css>
 
     Example: nukecss 'src/**/*.+(js|html)' 'src/**/*.css' > dist/minified.css
@@ -18,5 +17,5 @@ if (args.length !== 2) {
 glob.sync(args[1]).forEach(file => {
   const content = fs.readFileSync(file, 'utf8')
   const nuked = nuke('file://' + args[0], content)
-  console.log(nuked)
+  process.stdout.write(nuked)
 })
